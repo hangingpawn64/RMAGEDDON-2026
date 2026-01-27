ee70ad3dffaa8d2de5febf01b228b4f58ec4fd3c
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-01-28 01:17:32 +0530

    game names added

--- src/pages/Events/Robowar.jsx
+++ src/pages/Events/Robowar.jsx
@@ -95,7 +95,7 @@ const Robowar = () => {
 
           <div className="robowar-event-cards" data-aos="fade-left">
             <GameCard
-              title="Robo Race"
+              title="Vinewood Velocity"
               description="Unleash Speed, Strategy, and Skill! Push Your Robots to the Limit and Cross the Finish Line Victorious!"
               image="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1768246819/7e76f4fc-b6f8-4ff8-a286-b3319ee68121.png"
               onAction={handlePopupTrigger}
@@ -103,7 +103,7 @@ const Robowar = () => {
           </div>
           <div className="robowar-event-cards" data-aos="fade-right">
             <GameCard
-              title="Robo Soccer"
+              title="Demolitio Strike"
               description="Lead your robots to victory, score goals, and show off your skills in an action-packed Robo Soccer battle."
               image="https://res.cloudinary.com/ddcuqfkyt/image/upload/v1768583356/robosoccer_scmc08.jpg"
               onAction={handlePopupTrigger}
@@ -117,7 +117,7 @@ const Robowar = () => {
         <div className="robowar-event-container">
           <div className="robowar-event-cards" data-aos="fade-left">
             <GameCard
-              title="Robo Sumo"
+              title="Torque Cartel"
               description="Unleash your robots, push opponents out, and dominate the arena in Robo Sumo showdown!"
               image="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1768246871/de83b2a7-fb6b-435f-a9da-1b5aeb48a27b.png"
               onAction={handlePopupTrigger}
@@ -125,7 +125,7 @@ const Robowar = () => {
           </div>
           <div className="robowar-event-cards" data-aos="fade-right">
             <GameCard
-              title="Line Follower"
+              title="Track Theft Auto"
               description="Program Robot's to Follow the Path with Precision and Speed! Conquer the Line Following Challenge"
               image="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1768246887/8605c591-86e9-49a2-afc0-31782058b71d.png"
               onAction={handlePopupTrigger}
